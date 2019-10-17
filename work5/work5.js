e8d0775b7c90801da8d89d7505f40537003b6cf5
luohuolian <785615882@qq.com>
2019-10-17 11:35:17 +0800

    本明

--- work5/work5.js
+++ work5/work5.js
@@ -1,15 +1,35 @@
-//9x9的空表，请自行编写这段代码
-
-//倒九九乘法空表
-str = '<table border="1">'
-for(let i=9;i>0;--i){
+//9X9的空表
+let str = '<table border="1">'
+for(let i=9;i<18;i++){
     str += '<tr>'
-    for(var j=i;j>0;--j){
-        str += '<td>&nbsp；</td>'
+    for(var j=1;j<=9;++j){
+        str += '<td>&nbsp;</td>'
     }
     str +='</tr>'
 }
-str += '</table>'
-document.getElementById('table2').innerHTML = str
+str +='</table>'
+document.getElementById('table1').innerHTML =str
 
-//倒九九乘法表 ， 请自行编写这段代码
+//倒九九乘法空表
+str = '<table border="2">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str += '<td>&nbsp;</td>'
+    }
+    str += '</tr>'
+}
+str += '</table>'
+document.getElementById('table2').innerHTML =str
+
+//倒九九乘法表
+str = '<table border="3">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+        str += '<td>'+ j + '*' +i +'=' +(j*i)+'</td>'
+    }
+    str += '</tr>'
+}
+str += '</table>'
+document.getElementById('table3').innerHTML =str
